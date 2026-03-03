da2590db77bfeea8836fe831ab8b08fcd2934ff5
swastideep-maharana <swastideep67maharana@gmail.com>
2026-03-03 11:18:48 +0530

    project stup, auth & database

---
+++ src/lib/env.ts
@@ -0,0 +1,17 @@
+import { z } from "zod";
+import { createEnv } from "@t3-oss/env-nextjs";
+
+export const env = createEnv({
+    server: {
+        DATABASE_URL: z.string().min(1),
+        // APP_URL: z.string().min(1),
+        // R2_ACCOUNT_ID: z.string().min(1),
+        // R2_ACCESS_KEY_ID: z.string().min(1),
+        // R2_SECRET_ACCESS_KEY: z.string().min(1),
+        // R2_BUCKET_NAME: z.string().min(1),
+        // CHATTERBOX_API_URL: z.url(),
+        // CHATTERBOX_API_KEY: z.string().min(1),
+    },
+    experimental__runtimeEnv: {},
+    skipValidation: !!process.env.SKIP_ENV_VALIDATION,
+});
